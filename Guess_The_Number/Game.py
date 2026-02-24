a3cf77a22d987d5dafb87c4edde7779866695d2e
jigarrambhiya1 <jigarrambhiya1@gmail.com>
2026-02-25 05:24:53 +0530

    updated the Guess_The_Number Game

--- Guess_The_Number/Game.py
+++ Guess_The_Number/Game.py
@@ -21,14 +21,35 @@ def computer_guess(x):
             guess = random.randint(low, high)
         else:
             guess = low # could be high as high == low
+        
         feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower()
+        
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
             low = guess + 1
+        elif feedback == 'c':
+            pass
+        else:
+            print("Wrong Input.")
+        
+
 
     print('The Computer Guessed Your Number.')
 
 
-guess(100)
-computer_guess(99)
+
+print("Welcome to Guess the Number Game!")
+
+while True:
+    print("1. You want to Guess the Number. OR\n2. Let computer Guess the Number.")
+    option = int(input("Pick one Option OR (0 = Exit):. "))
+
+    if option == 1:
+        guess(100)
+    elif option == 2:
+        computer_guess(99)
+    elif option == 0:
+        break
+    else:
+        print("You Picked Wrong Number...")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-games
-from Youtube Kylie-Yang
+From Youtube Kylie-Yang
+Link:- https://www.youtube.com/watch?v=8ext9G7xspg
